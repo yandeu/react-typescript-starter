f2d429a002446b2728f7f5adbc705db92bfd0671
Yannick <20306025+yandeu@users.noreply.github.com>
2019-02-21 23:46:10 +0100

    Restructuring

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
-import './index.css'
-import App from './App'
+import './styles.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>react app</h1>
+      <img id="logo" src="public/img/React-icon.svg" />
+    </div>
+  )
+}
 
 render(<App />, document.getElementById('root'))
